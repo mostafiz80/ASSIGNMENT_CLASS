0c0e1b77b0512ef1e49e113e74bfc9589610bc44
mostafiz80 <38610334+mostafiz80@users.noreply.github.com>
2024-04-02 16:54:08 +0300

    gfhf

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,31 +1,31 @@
 // Organic javascript code ;)
 // Function that display result value 
 function getInputAndCalculate() {
-    let FirstInput = document.getElementsByName("inputnumber_first").value;
-    let SecondInputs = document.getElementsByName("inputnumber_second").value;
-    let Operator = document.getElementsByName("operator").value;
+    let FirstInput = parseInt(document.getElementById("inputnumber_first").value);
+    let SecondInputs = parseInt(document.getElementById("inputnumber_second").value);
+    let Operator = document.getElementById("operator").value;
     var result = calculation(FirstInput, SecondInputs, Operator);
     document.getElementById("displayresult").innerHTML = "Result "+result;
+    document.getElementById("calculator").reset();
 }
 
 function calculation(firstInput, secondInputs, operator){
     let result;
     switch(operator) {
         case "addition":
-            result = (firstInput + secondInputs)
+            result = (firstInput + secondInputs);
 
           break;
         case "subtraction":
-            result =  (firstInput - secondInputs)
+            result =  (firstInput - secondInputs);
 
           break;
         case "multiplication":
-            result =  (firstInput * secondInputs)
+            result =  (firstInput * secondInputs);
 
           break;
         case "division":
-            result =  (firstInput / secondInputs)
-       
+            result =  (firstInput / secondInputs);
           break;
 
       }
@@ -34,7 +34,7 @@ function calculation(firstInput, secondInputs, operator){
       
 }
 function resetform(){
-    document.getElementById("calculator").reset();
+    
     document.getElementById("displayresult").reset();
 }
 
